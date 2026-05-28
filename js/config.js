e825e9bf220b815123d7e93bd888fbd23d9dd087
bichitosshop <bichitosshopmdz@gmail.com>
2026-05-27 23:19:39 -0300

    remove: admin panel, config-loader, site-config (keep products & logo)

--- js/config.js
+++ js/config.js
@@ -1,7 +1,1 @@
 const SHEET_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vS4fDV7LGcWVh7JVEoo2h98RtZKJ4oCW66q4E9k-d3S9KcHMpW_toGJ-rLzeJwjLJKljpb7r3Q0-53C/pub?output=csv';
-
-// GitHub repo (usada por el panel admin para guardar cambios)
-const GITHUB_OWNER = 'bichitosshop';
-const GITHUB_REPO  = 'bichitosonline';
-const GITHUB_BRANCH = 'main';
-const PRODUCTOS_JSON_PATH = 'productos.json';
